6168cbaebdfee64aa8a4d476376f4a169df31272
MaikTB <maik@techbude.com>
2025-11-14 15:50:51 +0100

    Initial Commit

---
+++ AudioDeviceControl/AudioDeviceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioDeviceControlApp.swift
+//  AudioDeviceControl
+//
+//  Created by Maik Brüggemann on 14.11.25.
+//
+
+import SwiftUI
+
+@main
+struct AudioDeviceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
